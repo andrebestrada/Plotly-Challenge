acad2793103b0e054534fa57f5b6e1d74e32b68d
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-06-28 22:57:41 -0500

    New html

--- static/js/app.js
+++ static/js/app.js
@@ -12,6 +12,8 @@ d3.json("../../samples.json").then((importedData)=>{
         var options = d3.select("#selDataset").append("option");
         options.text(names[i])
     }
+    
+    d3.select("#count").text(names.length);
 
     // Call updatePlotly() when a change takes place to the DOM
     d3.selectAll("#selDataset").on("change", updateDashboard);
@@ -33,17 +35,16 @@ d3.json("../../samples.json").then((importedData)=>{
         var location = filtered_metaData.map(sample=>sample.location)
         var bbtype = filtered_metaData.map(sample=>sample.bbtype)
         
+        d3.select("#ethnicity").text(ethnicity);
+        d3.select("#location").text(location);
 
-
-        for (var i = 0; i < filtered_metaData.length; i++) { // the plainest of array loops
+        for (var i = 0; i < filtered_metaData.length; i++) {
             var obj = filtered_metaData[i];
             for (var key in obj) { 
                 var panel = d3.select("#sample-metadata").append("li")
                 panel.text(`${key}: ${obj[key]}`)
-                console.log(key, obj[key])
             }
         }
-
 
         filtered_data=Object(otus_data.samples).filter(sample=>sample.id===selector_id)
         var ids = Object(filtered_data).map(row => row.otu_ids)[0];
@@ -52,12 +53,22 @@ d3.json("../../samples.json").then((importedData)=>{
 
         ids_name = ids.map(otu_id => {return "OTU "+ otu_id});
         
+        d3.select("#otus_count").text(ids.length);
+        d3.select("#subject_id").text(selector_id);
+
+        var cultures_found=values.reduce((a, b) => a + b, 0)
+        d3.select("#cultures-found").text(cultures_found)
+        d3.select("#greatest-found").text(Math.max.apply(Math, values))
+        d3.select("#smallest-found").text(Math.min.apply(Math, values))
+
         var traceBar = {
             x: values.slice(0,10),
             y: ids_name.slice(0,10),
             text:text.slice(0,10),
             name: "OTUS",
             type: "bar",
+            marker: {
+              color: "#0d6efd"},
             orientation: "h"
         };
 
@@ -65,19 +76,31 @@ d3.json("../../samples.json").then((importedData)=>{
         var data_bar = [traceBar];
 
         // Define the plot layout
-        var layout_bar = {
+        var layout_bar = {            
             xaxis: { 
-                title: "x title" 
+                // title: "x title" 
             },
             yaxis: { 
                 autorange : "reversed"
-            }
+            },
+            height: 300,
+            width: 300,
+            margin: {
+              l: 80,
+              r: 20,
+              b: 20,
+              t: 10,
+              pad: 3
+            },
+            font:{
+              family: 'Roboto, sans-serif'
+            }, 
+
         };
 
         // Plot bar chart"
-        Plotly.newPlot("bar", data_bar, layout_bar);
+        Plotly.newPlot("bar", data_bar, layout_bar,{displayModeBar: false});
 
-        // Create trace and layout for bubble chart
         var trace_bubble = {
             x: ids,
             y: values,
@@ -85,57 +108,73 @@ d3.json("../../samples.json").then((importedData)=>{
             mode: 'markers',
             marker: {
             color: ids,
-            size: values
+            size: values,
+            colorscale: 'YlGnBu'   
             }
         };
     
         var data_bubble = [trace_bubble];
         
         var layout_bubble = {
-            title: 'Bubble Chart Hover Text',    
-            width: 600, height: 300    
+            xaxis: {title:"OTU ID"},
+            height: 260,
+            width: 700,
+            margin: {
+              l: 40,
+              r: 20,
+              b: 40,
+              t: 20,
+              pad: 4
+            }
+              
         };
-
-        console.log(data)
+        
+        
         Plotly.newPlot('bubble', data_bubble, layout_bubble);
-        // End of Bubble chart
 
         var wfreq = filtered_metaData.map(sample=>sample.wfreq)[0]
-        console.log(wfreq)
+        
 
-        var data = [
+        var data_gauge = [
             {
               domain: { x: [0, 1], y: [0, 1] },
               value: wfreq,
-              title: { text: "Belly Button Washing Frequency" },
               type: "indicator",
               mode: "gauge+number",
+              
 
               gauge: {
+                bar:{color: "#0d6efd",
+                  thickness:1},
                 axis: {range: [null, 9] },
                 steps: [
                   { range: [0, 1], color: "#FFFFFF" },
-                  { range: [1, 2], color: "#ccc5df" },
-                  { range: [2, 3], color: "#beb1e4" },
-                  { range: [3, 4], color: "#af9ee9" },
-                  { range: [4, 5], color: "#9e8aed" },
-                  { range: [5, 6], color: "#8a77f2" },
-                  { range: [6, 7], color: "#7364f6" },
-                  { range: [7, 8], color: "#5552f9" },
-                  { range: [8, 9], color: "#1c3ffd" },
+                  { range: [1, 2], color: "#FFFFFF" },
+                  { range: [2, 3], color: "#FFFFFF" },
+                  { range: [3, 4], color: "#FFFFFF" },
+                  { range: [4, 5], color: "#FFFFFF" },
+                  { range: [5, 6], color: "#FFFFFF" },
+                  { range: [6, 7], color: "#FFFFFF" },
+                  { range: [7, 8], color: "#FFFFFF" },
+                  { range: [8, 9], color: "#FFFFFF" },
                 ],
-                threshold: {
-                  line: { color: "blue", width: 5 },
-                  thickness: 0.75,
-                  value: wfreq
-                }
               }
             }
           ];
           
-          var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+          var layout_gauge = { 
+            font:{family: 'Roboto, sans-serif'},  
+            height: 140,
+              margin: {
+                l: 20,
+                r: 20,
+                b: 0,
+                t: 15,
+                pad: 3
+              }
+            };
 
-          Plotly.newPlot('gauge', data, layout);
+          Plotly.newPlot('gauge', data_gauge, layout_gauge);
 
     }
 })
